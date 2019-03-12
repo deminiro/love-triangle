7d217cbef77f4b3367340fefcfa2dacbeef243c2
deminiro <43145879+deminiro@users.noreply.github.com>
2019-03-12 12:45:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,19 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0; 
+  function preferences(b,c,a){  
+         if( b = a + 1,
+             c = b + 1,
+             a = c - 2,
+             c.indexOf = b.indexOf + 1,
+             a.indexOf = c.indexOf + 1,
+             b.indexOf = a.indexOf - 2){
+             return count++;
+}
+  else{ 
+  return undefined;
+  };
+};
+  return count;
 };
