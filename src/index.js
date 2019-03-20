2ff4c8f41fdc5194d59862192fd3d13ba5816e3f
deminiro <43145879+deminiro@users.noreply.github.com>
2019-03-20 11:39:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,41 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation 
-  let count = 0; 
-  
-   if(preferences[0]+1===preferences[1] && preferences[1]-2===preferences[2] && preferences[2]-2===preferences[0]){
-      for(let count;count<=preferences.length;count++){
-      return preferences.splice(0,3) && ++count;
-    }
-   }
-   return count;
+  const workArray = [null, ...preferences];
+
+    const result = workArray.reduce(({ count, arr }, numberA, index) => {
+        const numberB = arr[numberA];
+
+        if (numberB && (numberB !== numberA)) {
+            const numberC = arr[numberB];
+
+            if (numberC && ((numberC !== numberB) && numberC !== numberA) && numberC === index) {
+                const arrCopy = [...arr];
+
+                arrCopy.splice(index, 1, null);
+                arrCopy.splice(numberB, 1, null);
+                arrCopy.splice(numberC, 1, null);
+                
+                return {
+                    count: ++count,
+                    arr: arrCopy
+                };
+            } else {
+                return {
+                    count,
+                    arr
+                };
+            }
+        } else {
+            return {
+                count,
+                arr
+            };
+        }
+    }, {
+        count: 0,
+        arr: workArray
+    });
+
+    return result.count;
   }
